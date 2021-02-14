dbc8a48470cf609617cfab19bc6e2e5f0df0dca6
Aldiyar Sakhan <aldiyar.sakhan@uco.kz>
2021-02-14 19:01:30 +0600

    hideShow draggable

--- lib/widgets/HomeScreen.dart
+++ lib/widgets/HomeScreen.dart
@@ -10,36 +10,69 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
-  String formattedDate = DateFormat('EEEE, d.MM.y','ru').format(DateTime.now());
-  String formattedTime = DateFormat('kk : mm','ru').format(DateTime.now());
+  String formattedDate = DateFormat('EEEE, d.MM.y', 'ru').format(
+      DateTime.now());
+  String formattedTime = DateFormat('kk : mm', 'ru').format(DateTime.now());
+  double _initialChildSize = 0.75;
+  bool hide = false;
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setInitialChildSize();
+    print(_initialChildSize);
+  }
+  void setInitialChildSize() {
+    setState(() {
+      if (hide) {
+        _initialChildSize = 0;
+        hide = false;
+        print(_initialChildSize);
+      } else if(!hide){
+        hide = true;
+        _initialChildSize = 0.75;
+        print(_initialChildSize);
+      }
+    });
+  }
 
+  @override
   Widget build(BuildContext context) {
-
     return Container(
-      height: MediaQuery.of(context).size.height,
+      height: MediaQuery
+          .of(context)
+          .size
+          .height,
       width: double.infinity,
       child: Stack(
         children: <Widget>[
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 32, vertical: 32),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(height: 20,),
-                Text(formattedTime, style: TextStyle(color: Colors.white70, fontSize: 45, fontWeight: FontWeight.bold),),
-                Text(formattedDate, style: TextStyle(color: Colors.white, fontSize: 16,),),
-                SizedBox(height: 20,)
-              ],
+          InkWell(
+            onTap: setInitialChildSize,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 32, vertical: 32),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: 20,),
+                  Text(formattedTime, style: TextStyle(color: Colors.white70,
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold),),
+                  Text(formattedDate,
+                    style: TextStyle(color: Colors.white, fontSize: 16,),),
+                  SizedBox(height: 20,)
+                ],
+              ),
             ),
           ),
           DraggableScrollableSheet(
-            builder: (context, scrollController){
+            builder: (context, scrollController) {
               return Container(
                   decoration: BoxDecoration(
                       color: Color.fromRGBO(243, 245, 248, 1),
-                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20))
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20))
                   ),
                   child: SingleChildScrollView(
                     child: Column(
@@ -49,10 +82,14 @@ class _HomeScreenState extends State<HomeScreen> {
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: <Widget>[
-                              Text("Обьекты", style: TextStyle(fontWeight: FontWeight.w900, fontSize: 24, color: Colors.black),),
+                              Text("Обьекты", style: TextStyle(
+                                  fontWeight: FontWeight.w900,
+                                  fontSize: 24,
+                                  color: Colors.black),),
                             ],
                           ),
-                          padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 32, vertical: 12),
                         ),
                         CategoryList(),
                       ],
@@ -61,8 +98,8 @@ class _HomeScreenState extends State<HomeScreen> {
                   )
               );
             },
-            initialChildSize: 0.75,
-            minChildSize: 0.75,
+            initialChildSize: _initialChildSize,
+            minChildSize: _initialChildSize,
             maxChildSize: 1,
           )
         ],
